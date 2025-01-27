a93bebf42d04ef6eb1e7ec759ae23704389f3816
ronanzindev <morellianogm@gmail.com>
2025-01-27 17:57:12 -0300

    removed unsed props

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils"
 import React from "react"
 
-const MainNav = ({className, ...props}: React.HTMLAttributes<HTMLElement>) => {
+const MainNav = ({className}: React.HTMLAttributes<HTMLElement>) => {
     return (
         <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
             <a className="text-sm  font-medium transition-colors hover:text-primary" href="/">Dashboard</a>
