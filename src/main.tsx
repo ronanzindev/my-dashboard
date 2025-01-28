12bb0fd31ed87a2d4f9482dbd3a59a6a97ff2934
ronanzindev <morellianogm@gmail.com>
2025-01-28 18:48:46 -0300

    change toast time to close

--- src/main.tsx
+++ src/main.tsx
@@ -18,7 +18,7 @@ createRoot(document.getElementById('root')!).render(
         <ThemeProvider>
           <ToastContainer
             position="top-right"
-            autoClose={5000}
+            autoClose={2000}
             hideProgressBar={false}
             newestOnTop={false}
             closeOnClick={false}
